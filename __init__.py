d19be6f23e5b455b0a1fd3d436937c9f6477f5a4
Christian Fobel <christian@fobel.net>
2019-08-01 14:26:48 -0400

    fix(step): add missing prompt dialog

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
+from asyncio_helpers import sync
 from logging_helpers import _L
 from microdrop.plugin_manager import (PluginGlobals, Plugin, IPlugin,
                                       implements)
+from pygtkhelpers.gthreads import gtk_threadsafe
+import gtk
+import microdrop as md
 import trollius as asyncio
 
 from ._version import get_versions
@@ -27,8 +31,55 @@ class WaitForAckPlugin(Plugin):
 
     @asyncio.coroutine
     def on_step_run(self, plugin_kwargs, signals):
-        _L().debug('doing some work when a step is run')
-        raise asyncio.Return()
+        '''Wait for operator to manually acknowledge that step is complete.
+
+        Display a GTK dialog with **OK** and **Cancel** buttons.  If **Cancel**
+        is pressed, raise a ``RuntimeError`` exception is raised.  If **OK** is
+        pressed, finish step execution.
+
+        Parameters
+        ----------
+        message : str
+            Message displayed to user in dialog.
+        description : str, optional
+            Title of the prompt (if specified).
+
+        Raises
+        ------
+        RuntimeError
+            If **Cancel** button is pressed.
+        '''
+        message = 'Click OK to continue or Cancel to stop protocol'
+
+        # Use `asyncio_helpers.sync` decorator to:
+        #  1. Launch dialog from main GTK thread (through `gtk_threadsafe`)
+        #  2. Provide an asyncio wrapper around async GTK call, which returns
+        #     only after the scheduled call has finished executing. This makes
+        #     it possible to coordinate between UI code and protocol asyncio code.
+        @sync(gtk_threadsafe)
+        def _acknowledge():
+            app = md.app_context.get_app()
+            parent_window = app.main_window_controller.view
+            dialog = gtk.MessageDialog(parent=parent_window,
+                                       message_format=message,
+                                       type=gtk.MESSAGE_OTHER,
+                                       buttons=gtk.BUTTONS_OK_CANCEL)
+            # Increase default dialog size.
+            dialog.props.title = 'Wait for acknowledgement'
+            dialog.props.use_markup = True
+
+            response_code = dialog.run()
+            dialog.destroy()
+            return response_code
+
+        _L().debug('wait for user to acknowledge step completion')
+
+        # Queue dialog to be launched in GTK thread and wait for response.
+        response = yield asyncio.From(_acknowledge())
+        if response != gtk.RESPONSE_OK:
+            raise RuntimeError('Cancelled in response to message `%s`.' % message)
+        else:
+            _L().debug('user acknowledged step completion')
 
 
 PluginGlobals.pop_env()
